b24513d118eb6a9f1f440bc2d798279eaa6fc4ee
dorick <dorick2301@mail.ru>
2026-04-07 19:20:18 +0300

    homework

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,10 +1,4 @@
 import unittest
-import sys
-import os
-
-# Добавляем путь к src
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from src.Product import Product
 
 
